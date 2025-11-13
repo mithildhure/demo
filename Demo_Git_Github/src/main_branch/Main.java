73e0bf9a0e8897b37d2d1c711c08baedbe19c413
Lenovo <Lenovo@DESKTOP-MOB8HJ5>
2025-11-13 19:57:07 +0530

    Commit Succesfull

---
+++ Demo_Git_Github/src/main_branch/Main.java
@@ -0,0 +1,24 @@
+package main_branch;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Alright a demo code to see the working of git
+		
+		/* 
+		 * 	using the demo repo
+		 * 	cloning repo through url
+		 * 
+		 * 	creating a PAT(personal access tokens)
+		 * 	for username and password 
+		 * 	not giving expiry to the token
+		 * 
+		 * */
+		
+		System.out.println("Hello github...");
+		
+	}
+
+}
