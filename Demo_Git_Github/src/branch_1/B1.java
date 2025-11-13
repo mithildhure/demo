fb43724496614cd04e98e48de8d8878bddbaa628
Lenovo <Lenovo@DESKTOP-MOB8HJ5>
2025-11-13 21:21:34 +0530

    commit in new branch succesfull

---
+++ Demo_Git_Github/src/branch_1/B1.java
@@ -0,0 +1,23 @@
+package branch_1;
+
+public class B1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/* New Branch
+		 * 
+		 * Team -> Switch to -> New Branch
+		 * 
+		 * Source -> main
+		 * branch-> branch1
+		 * 
+		 * tick configure upstream for push and pull, select Merge
+		 * 
+		 * 
+		 * 
+		 * */
+		
+	}
+
+}
